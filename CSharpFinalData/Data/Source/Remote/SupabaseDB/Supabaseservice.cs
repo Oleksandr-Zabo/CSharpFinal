c5ebbbd3322f363761e5dc057647fe6f2b144f38
Oleksandr-Zabo <zoleksandr06@gmail.com>
2025-05-05 20:52:51 +0300

    Add SupabaseService and configure project dependencies
    Introduce the `SupabaseService` class for handling Supabase integration, including initialization and user management. Update project files to include the necessary Supabase NuGet package and add project references to establish dependency relationships.

---
+++ CSharpFinalData/Data/Source/Remote/SupabaseDB/Supabaseservice.cs
@@ -0,0 +1,45 @@
+using Supabase.Gotrue;
+using Client = Supabase.Client;
+using static Supabase.Postgrest.Constants;
+
+namespace CSharpFinalData.Data.Source.Remote.SupabaseDB;
+
+public class SupabaseService
+{
+    private const string SupabaseUrl = "https://blsbwhilzmlhlhxywfpl.supabase.co";
+    private const string SupabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsc2J3aGlsem1saGxoeHl3ZnBsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5NDg0MTYsImV4cCI6MjA2MTUyNDQxNn0.QN24DqtBr6wFqHNyAYw-XOoHGxbxx0fneOoDJxFsDHo";
+    
+    private readonly Supabase.Client _client;
+        
+    public User? SupabaseUser { get; set; } = null;
+        
+    public bool IsLoggedIn { get; set; } = false;
+        
+    public SupabaseService()
+    {
+        try
+        {
+            var options = new Supabase.SupabaseOptions
+            {
+                AutoConnectRealtime = true,
+            };
+            _client = new Client(SupabaseUrl, SupabaseKey, options);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"SupabaseService() raise Exception: {ex.Message}");
+        }
+    }
+        
+    public async Task InitServiceAsync()
+    {
+        try
+        {
+            await _client.InitializeAsync()!;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"InitServiceAsync() raise Exception: {ex.Message}");
+        }
+    }
+}

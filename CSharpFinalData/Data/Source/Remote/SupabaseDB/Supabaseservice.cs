852951b7a9e8d439c0d09832e2c9569d78fcd1e5
Oleksandr-Zabo <zoleksandr06@gmail.com>
2025-05-05 20:53:56 +0300

    developer branch

--- CSharpFinalData/Data/Source/Remote/SupabaseDB/Supabaseservice.cs
+++ CSharpFinalData/Data/Source/Remote/SupabaseDB/Supabaseservice.cs
@@ -4,8 +4,16 @@ using static Supabase.Postgrest.Constants;
 
 namespace CSharpFinalData.Data.Source.Remote.SupabaseDB;
 
+/// <summary>
+/// Provides services to interact with Supabase.
+/// </summary>
+/// <remarks>
+/// This class handles initialization and user session management for the Supabase client.
+/// It is designed to work with a specific Supabase project's URL and API key.
+/// </remarks>
 public class SupabaseService
 {
+    
     private const string SupabaseUrl = "https://blsbwhilzmlhlhxywfpl.supabase.co";
     private const string SupabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsc2J3aGlsem1saGxoeHl3ZnBsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5NDg0MTYsImV4cCI6MjA2MTUyNDQxNn0.QN24DqtBr6wFqHNyAYw-XOoHGxbxx0fneOoDJxFsDHo";
     
